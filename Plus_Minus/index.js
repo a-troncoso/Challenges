9e96ebf82df26eb53f8054867abed6b6875b7e17
a-troncoso <alvaro.mc2@gmail.com>
2020-11-23 23:58:55 -0300

    challenge plusMinus added

---
+++ Plus_Minus/index.js
@@ -0,0 +1,25 @@
+/**
+ * Function that prints the ratios of its elements that are positive, negative and zero
+ *
+ * For more details:
+ * {@link https://www.hackerrank.com/challenges/plus-minus}
+ *
+ * @param {Array.<number>} arr Array of numbers
+ * @return {number}
+ */
+function plusMinus(arr) {
+  const length = arr.length
+  const qPositives = arr.filter(n => n > 0).length
+  const qNegatives = arr.filter(n => n < 0).length
+  const qZeros = arr.filter(n => n === 0).length
+
+  const positiveProportion = (qPositives / length).toFixed(6)
+  const negativeProportion = (qNegatives / length).toFixed(6)
+  const zeroProportion = (qZeros / length).toFixed(6)
+
+  console.log(positiveProportion)
+  console.log(negativeProportion)
+  console.log(zeroProportion)
+}
+
+console.log(plusMinus([-4, 3, -9, 0, 4, 1, 0]))
--- README.md
+++ README.md
@@ -6,6 +6,7 @@ This repo hosts my solutions for HackeRank challenges
 - Compare the Triplets: [https://www.hackerrank.com/challenges/compare-the-triplets/]()
 - A Very Big Sum: https: [//www.hackerrank.com/challenges/a-very-big-sum/]()
 - Diagonal Difference: [https://www.hackerrank.com/challenges/diagonal-difference/]()
+- Plus Minus: [https://www.hackerrank.com/challenges/plus-minus/]()
 
 ---
 
